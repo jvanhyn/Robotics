5772dee3f3731d86b2bd1369f679eb924620e892
jvanhyning <83515204+jvanhyning@users.noreply.github.com>
2024-02-20 11:50:30 -0800

    Update homework5.m

--- homework5.m
+++ homework5.m
@@ -1,14 +1,14 @@
 addpath("/Users/jvanhyn/Documents/GitHub/Robotics/mr");
 syms L theta_1 theta_2 theta_3 real
-theta_1 = 0;
-theta_2 = 0;
-theta_3 = 0;
+theta_1 = deg2rad(0);
+theta_2 = deg2rad(45);
+theta_3 = deg2rad(-45);
 
 S1 = [0,0,1,0,0,0]';
 S2 = [0,-1,0,2*L,0,0]';
 S3 = [0,-1,0,2*L,0,-L]';
 
-B1 = [0,0,1,0,0,0]';
+B1 = [0,0,1,-L,2*L,0]';
 B2 = [0,-1,0,0,0,-2*L]';
 B3 = [0,-1,0,0,0,-L]';
 
@@ -16,14 +16,12 @@ Js1 = S1;
 Js2 = Adjoint(expm(VecTose3(S1)*theta_1))*S2;
 Js3 = Adjoint(expm(VecTose3(S1)*theta_1)*expm(VecTose3(S2)*theta_2))*S3;
 
-Jb1 = B1;
-Jb2 = Adjoint(expm(VecTose3(-B1)*theta_1))*B2;
-Jb3 = Adjoint(expm(VecTose3(-B1)*theta_1)*expm(VecTose3(-B2)*theta_2))*B3;
+Jb1 = Adjoint(expm(VecTose3(-B3)*theta_1)*expm(VecTose3(-B2)*theta_2))*B1;
+Jb2 = Adjoint(expm(VecTose3(-B3)*theta_1))*B2;
+Jb3 = B3;
 
 Js = [Js1,Js2,Js3];
 Jb = [Jb1,Jb2,Jb3];
-Vs = Js*[0;5/L;0]
-Vb = Jb*[0;5/L;0]
 
 
 
