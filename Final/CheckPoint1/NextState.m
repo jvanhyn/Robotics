6ba58ba31ccd22556fae34a490d1f9dc10f64358
Ananya-odi <84366414+Ananya-odi@users.noreply.github.com>
2024-03-10 11:26:50 -0700

    Add files via upload

--- Final/CheckPoint1/NextState.m
+++ Final/CheckPoint1/NextState.m
@@ -1,24 +1,59 @@
-function [q,theta,u] = NextState(q0,u0,theta0,du,dtheta,dt,du_max)
+function [q,theta,u] = NextState(q0,u0,theta0,du,dtheta,dt,speed_max)
+
+% NextState uses the velocity kinematics of a 5R mobile robot and the Euler method
+% of approximation to predict how the robot will move in a small timestep
+% given its current configuration and velocity.
+
+% INPUTS:
+% q0: current location of the chassis (in the set of R^3)
+% u0: current chassis wheel angles (in the set of R^4)
+% theta0: current joint angles (in the set of R^5)
+
+% du: joint velocities (in the set of R^5)
+% dtheta: wheel velocities (in the set of R^4)
+
+% dt: timestep
+% dspeed_max: speed threshold (applies to both wheel and joint speeds)
+
+% OUTPUTS: configuration of the next state
+% q: next location of the chassis (in the set of R^3)
+% u: next chassis wheel angles (in the set of R^4)
+% theta: next joint angles (in the set of R^5)
+
+    % ensure that wheel speeds are under the speed threshold
     for i = 1:4
-        if(abs(du(i)) > du_max)
-            du(i) = du_max;
+        if(abs(du(i)) > speed_max)
+            du(i) = speed_max;
+        end
+    end
+% AT: added the following block and renamed du_max to speed_max (I believe we also need to cap the joint
+% vel.s)
+    % ensure that joint speeds are under the speed threshold
+    for i = 1:5
+        if(abs(dtheta(i)) > speed_max)
+            dtheta(i) = speed_max;
         end
     end
     
+    % Use Euler's method to approximate new wheel and joint angles
     u = u0 + du.*dt;
     theta = theta0 + dtheta.*dt;
 
+  % Calculate the next state of the chassis configuration using odometry
+    % AT: what are l,w,r?
     l = 1;
     w = 1;
     r = 1;
-
-    F = r/4 * [-1/(l+w) 1/(l+w) 1/(l+w) -1/(l+w); 1 1 1 1; -1 1 -1 1];
+    
+    % Calculate the body frame twist of the chassis
+    F = r/4 * [-1/(l+w) 1/(l+w) 1/(l+w) -1/(l+w); 1 1 1 1; -1 1 -1 1]; % F for a 4-mecanum-wheel chassis (see eqn 13.33 from mr)
     V_b =  F*du;
 
     wzb = V_b(1);
     vxb = V_b(2);
     vyb = V_b(3);
-
+    
+    % Extract change in coordinates relative to the chassis body frame
     if wzb == 0 
         dqb = [wzb;vxb;vyb];
     else 
@@ -27,8 +62,9 @@ function [q,theta,u] = NextState(q0,u0,theta0,du,dtheta,dt,du_max)
               (vyb*sin(wzb)+vxb*(1-cos(wzb)))/wzb];
     end
 
-    phi = q0(1);
-    dqs = [1 0 0; 0 cos(phi) -sin(phi); 0 sin(phi) cos(phi)]*dqb;
+    phi = q0(1); % current chassis angle
+    dqs = [1 0 0; 0 cos(phi) -sin(phi); 0 sin(phi) cos(phi)]*dqb; %transform dq from frame {b} to frame {s}
 
+    % Estimate new chassis configuration
     q = q0 + dqs * dt;
 end
