24b2c77c111a2d6a16a71c5e6f93bc422b877e16
jvanhyning <45504971+jvanhyn@users.noreply.github.com>
2024-03-12 14:28:32 -0700

    c

--- Final/CheckPoint1/TrajectoryGenerator.m
+++ Final/CheckPoint1/TrajectoryGenerator.m
@@ -30,25 +30,28 @@ function trajectory = TrajectoryGenerator(Tse_i,Tsc_i,Tsc_f,Tce_g,Tce_s,k)
     % Define trajectory segments
     Traj1 = ScrewTrajectory(Tse_i,Tse_si,1,N,3);  % Initial      to     Standby-1
     Traj2 = ScrewTrajectory(Tse_si,Tse_gi,1,N,3); % Standby-1    to     Cube-Initial
-    Traj3 = ScrewTrajectory(Tse_gi,Tse_gi,1,N,3); % Cube-Initial to     Cube-Initial
+    Traj3 = ScrewTrajectory(Tse_gi,Tse_gi,1,63,3); % Cube-Initial to     Cube-Initial
     Traj4 = ScrewTrajectory(Tse_gi,Tse_si,1,N,3); % Cube-Initial to     Standby-1
     Traj5 = ScrewTrajectory(Tse_si,Tse_sf,1,N,3); % Standby-1    to     Standby-2
     Traj6 = ScrewTrajectory(Tse_sf,Tse_gf,1,N,3); % Standby-2    to     Cube-Final
-    Traj7 = ScrewTrajectory(Tse_gf,Tse_gf,1,N,3); % Cube-Final   to     Cube-Final
+    Traj7 = ScrewTrajectory(Tse_gf,Tse_gf,1,63,3); % Cube-Final   to     Cube-Final
     Traj8 = ScrewTrajectory(Tse_gf,Tse_sf,1,N,3); % Cube-Final   to     Standby-2
     
     % Reformat data for CopeliaSym
     for i = 1:N
         T1(i,:) = [Traj1{i}(1,1:3),Traj1{i}(2,1:3),Traj1{i}(3,1:3),Traj1{i}(1:3,4)',0]; 
-        T2(i,:) = [Traj2{i}(1,1:3),Traj2{i}(2,1:3),Traj2{i}(3,1:3),Traj2{i}(1:3,4)',0]; 
-        T3(i,:) = [Traj3{i}(1,1:3),Traj3{i}(2,1:3),Traj3{i}(3,1:3),Traj3{i}(1:3,4)',1]; 
+        T2(i,:) = [Traj2{i}(1,1:3),Traj2{i}(2,1:3),Traj2{i}(3,1:3),Traj2{i}(1:3,4)',0];  
         T4(i,:) = [Traj4{i}(1,1:3),Traj4{i}(2,1:3),Traj4{i}(3,1:3),Traj4{i}(1:3,4)',1]; 
         T5(i,:) = [Traj5{i}(1,1:3),Traj5{i}(2,1:3),Traj5{i}(3,1:3),Traj5{i}(1:3,4)',1]; 
         T6(i,:) = [Traj6{i}(1,1:3),Traj6{i}(2,1:3),Traj6{i}(3,1:3),Traj6{i}(1:3,4)',1];
-        T7(i,:) = [Traj7{i}(1,1:3),Traj7{i}(2,1:3),Traj7{i}(3,1:3),Traj7{i}(1:3,4)',0]; 
         T8(i,:) = [Traj8{i}(1,1:3),Traj8{i}(2,1:3),Traj8{i}(3,1:3),Traj8{i}(1:3,4)',0]; 
     end
-    
+
+    for i = 1:63
+        T3(i,:) = [Traj3{i}(1,1:3),Traj3{i}(2,1:3),Traj3{i}(3,1:3),Traj3{i}(1:3,4)',1];
+        T7(i,:) = [Traj7{i}(1,1:3),Traj7{i}(2,1:3),Traj7{i}(3,1:3),Traj7{i}(1:3,4)',0]; 
+    end
+
 % Concatanate trajectory segments
 trajectory = [T1;T2;T3;T4;T5;T6;T7;T8];
 
