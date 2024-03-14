10cc635575a819087f7b92556cb90dc615c4aa4c
jvanhyning <45504971+jvanhyn@users.noreply.github.com>
2024-03-14 12:51:13 -0700

    c

--- Final/CheckPoint3/FeedbackControl.m
+++ Final/CheckPoint3/FeedbackControl.m
@@ -1,8 +1,10 @@
 function [Vb,du,dtheta] = FeedbackControl(X,Xd,Xd1,Kp,Ki,dt)
-% brac_Xe = matrixLog6(Xd\X);
-% brac_Vd = 1/dt*MatrixLog6(Xd\Xd1)
-% Xe = se3ToVec(brac_Xe)
-% Vd = se3ToVec(brac_Vd)
+brac_Xe = MatrixLog6(X\Xd);
+brac_Vd = 1/dt*MatrixLog6(Xd\Xd1);
+Xe = se3ToVec(brac_Xe);
+Vd = se3ToVec(brac_Vd);
+Vb = Adjoint(X\Xd)*Vd+Kp*Xe+Ki*Xe*dt;
 
-% Vb = Adjoint(Xd\X)*Vd+Kp*Xe+Ki*Xe*dt
+
+
 end
--- Final/CheckPoint3/FeedbackTest.m
+++ Final/CheckPoint3/FeedbackTest.m
@@ -1,3 +1,6 @@
+close all; clear; clc; 
+addpath(cd,'../mr')
+
 Xd_i = [...
     0   0   1   0.5   ;
     0   1   0   0     ;
@@ -20,18 +23,18 @@ X = [...
     ];
 dt = 0.01;
 
-Kp = zeros(8,8);
-Ki = zeros(8,8);
+Kp = zeros(6,6);
+Ki = zeros(6,6);
 
-[Vb,du,dtheta] = FeedbackControl(X,Xd_i,Xd_f,Kp,Ki,dt);
+[Vd,AdjVd,Vb,Xe] = FeedbackControl(X,Xd_i,Xd_f,Kp,Ki,dt);
 
 
 %% Solutions
 du_dtheta_s = [157.2,157.2,157.2,157.2,0,-652.9,1398.6,-745.7,0];
 
-check1 = [0;0;0;20;0;10] == Vd;
-check2 = [0;0;0;21.409;0;6.455];
-check3 = [0, 0, 0, 21.409, 0, 6.455];
-check4 = [0, 0.171, 0, 0.080, 0, 0.107]
-check5 = du_dtheta_s == [du,dtheta]
+check1 = [0;0;0;20;0;10] == round(Vd,3)
+check2 = [0;0;0;21.409;0;6.455] == round(AdjVd,3)
+check3 = [0;0;0;21.409;0;6.455] == round(Vb,3)
+check4 = [0;0.171;0;0.080;0;0.107] == round(Xe,3)
+% check5 = du_dtheta_s == [du,dtheta]
 
