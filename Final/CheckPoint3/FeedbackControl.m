a6a3123a8a97cfdabf4cfee3ea11b62fe6f14ebb
Jonathan Van Hying <45504971+jvanhyn@users.noreply.github.com>
2024-03-21 15:22:32 -0700

    c

--- Final/CheckPoint3/FeedbackControl.m
+++ Final/CheckPoint3/FeedbackControl.m
@@ -11,30 +11,16 @@ function [du,dtheta,Vb,Xe,Xi] = FeedbackControl(theta,Tse,Tsd,Tsdn,Kp,Ki,Xi,dt,c
 % Kp: proportial control gain  (in the set of R^6x6)
 % Ki: proportial integral gain (in the set of R^6x6)
 
+% control: 1X3 vector of logical values that toggle 
+
 % OUTPUTS: configuration of the next state
 % Vb: commanded body twist                      (in the set of R^6)
 % du: commanded wheel velocities                (in the set of R^4)
 % theta: commanded manioulator joint velocities (in the set of R^5)
 
-Ted = Tse\Tsd;                      % Transformation from {d} to {e}
-Tddn = Tsd\Tsdn;                    % Transformation from {d} to {dn}
-
-brac_Xe = MatrixLog6(Ted);        % Error Twist Xe as Matrix 
-Xe = se3ToVec(brac_Xe);         % Error Twist Xe      
-
-brac_Vd = 1/dt * MatrixLog6(Tddn); % Desired Twist Vd as Matrix 
-Vd = se3ToVec(brac_Vd);            % Desired Twist Vd 
-
-Xi = Xi + Xe*dt;                   % integrate the error over the next time step
-
-control_ff = control(1)*Adjoint(Ted)*Vd;      % Feed Forward control component 
-control_p = control(2)*Kp*Xe;               % Proportional control component 
-control_i = control(3)*Ki*Xi;            % Integral control component 
-
-Vb = control_ff + control_p + control_i;  % Commanded control twist
-
+%% Robot Info
 % Default Configurations 
-M0e =  [1 0 0 0.0330; 0 1 0 0; 0 0 1 0.6546; 0 0 0 1]; % default end effector configuration
+M0e =  [1 0 0 0.0330; 0 1 0 0; 0 0 1 0.6546; 0 0 0 1]; % default end effector to base configuration
 Tb0 =  [1 0 0 0.1667; 0 1 0 0; 0 0 1 0.0026; 0 0 0 1]; % manipulator base to body frame transformation
 
 % Manipulator screw axes
@@ -45,32 +31,51 @@ B4 = [0, -1,  0, -0.2176,      0,  0]';
 B5 = [0,  0,  1,       0,      0,  0]';
 Blist = [B1,B2,B3,B4,B5];
 
-T0e = FKinBody(M0e,Blist,theta); % End-effector to manipulator base transformation
-Jm = JacobianBody(Blist,theta);
+% manipulator effector to base transformation 
+T0e = FKinBody(M0e,Blist,theta);
 
+% Chasis dimensions 
 l = 0.47/2;
 w = 0.3/2;
 r = 0.0475;
 
-F6 = r/4 * [0 0 0 0; 0 0 0 0; -1/(l+w) 1/(l+w) 1/(l+w) -1/(l+w); 1 1 1 1; -1 1 -1 1; 0 0 0 0]; % F for a 4-mecanum-wheel chassis (see eqn 13.33 from mr)
-Jbase = Adjoint(inv(T0e)*inv(Tb0))*F6;
+% F for a 4-mecanum-wheel chassis (see eqn 13.33 from mr)
+F6 = r/4 * [0 0 0 0; 0 0 0 0; -1/(l+w) 1/(l+w) 1/(l+w) -1/(l+w); 1 1 1 1; -1 1 -1 1; 0 0 0 0]; 
 
+%% Caculate Error 
+Ted = Tse\Tsd;                      % Transformation from {d} to {e}
+Tddn = Tsd\Tsdn;                    % Transformation from {d} to {dn}
+
+brac_Vd = 1/dt * MatrixLog6(Tddn);  % Desired Twist Vd as Matrix 
+Vd = se3ToVec(brac_Vd);             % Desired Twist Vd 
+
+brac_Xe = MatrixLog6(Ted);          % Error Twist Xe as Matrix 
+Xe = se3ToVec(brac_Xe);             % Error Twist Xe      
+
+Xi = Xi + Xe*dt;                    % Integral of Error Twist Xi
+
+%% Control Law
+control_ff = Adjoint(Ted)*Vd;    % Feed Forward control component 
+control_p  = Kp*Xe;              % Proportional control component 
+control_i  = Ki*Xi;              % Integral control component 
+
+% Toggle control options
+control_ff = control(1)*control_ff;   % toggle Feed Forward control
+control_p  = control(2)*control_p;    % toggle Proportional control  
+control_i  = control(3)*control_i;    % toggle Integral control 
+
+% Commanded Twist
+Vb = control_ff + control_p + control(3)*control_i;   
+
+%% Jacobians
+Jm = JacobianBody(Blist,theta);
+Jbase = Adjoint(inv(T0e)*inv(Tb0))*F6;
 J = [Jbase,Jm];
+
+%% Joint Velocities
 Q = pinv(J,1e-3)*Vb;
+
 dtheta = Q(5:end);
 du = Q(1:4);
 
-% if rank(Jm,1e-3) < 5
-%     dtheta = zeros(5,1);
-% end
-
-
-%% Solutions
-% check1 = [0;0;0;20;0;10] - round(Vd,3)
-% check2 = [0;0;0;21.409;0;6.455] - round(control_ff,3)
-% check3 = [0;0;0;21.409;0;6.455] - round(Vb,3)
-% check4 = [0;0.171;0;0.080;0;0.107] - round(Xe(:,end),3)
-% check5 = [157.2,157.2,157.2,157.2,0,-652.9,1398.6,-745.7,0]' - [du;dtheta]
-% check6 = [0, 0.171, 0, 21.488, 0, 6.562]' - Vb
-% check7 = [157.5, 157.5, 157.5, 157.5, 0, -654.3, 1400.9, -746.8, 0]' - [du;dtheta]
 end
--- Final/CheckPoint4/SimulateRobot.m
+++ Final/CheckPoint4/SimulateRobot.m
@@ -1,4 +1,4 @@
-close all; clear; clc;
+% close all; clear; clc;
 
 addpath(cd,'../mr')
 addpath(cd,'../CheckPoint1')
@@ -7,25 +7,26 @@ addpath(cd,'../CheckPoint3')
 
 %% SimulateRobot
 %{
-    SimulateRobot simulates the mobile manipulation of a cube using of a KUKA youBot. 
-    The youBot follows a predetermined trajectorty 
+    SimulateRobot simulates the mobile manipulation of a cube using a KUKA youBot.
+    A Feed Forward + PI contoller guids the robot along a pregenerated trajectory.
+    The trajectory outlines a path from the robot to initial and final positions of the cube.
     This script uses the following functions
 
-    -> TrajectoryGenerator: 
-        takes waypoints 
-        generates a desired trajectory 
+    -> TrajectoryGenerator:
+        takes waypoints
+        generates a desired trajectory
 
-    -> FeedbackControl: 
-        takes desired trajectory 
+    -> FeedbackControl:
+        takes desired trajectory
         generates motor velocity commands
 
-    -> NextState: 
-        takes velocity commands 
+    -> NextState:
+        takes velocity commands
         generates change in position over timestep dt
 %}
 
 % Helper Transformation Matrix Functions
-Tx = @(x,y,z,phi) [1         0        0        x; 0         cos(phi) -sin(phi) y; 0         sin(phi) cos(phi) z; 0 0 0 1]; 
+Tx = @(x,y,z,phi) [1         0        0        x; 0         cos(phi) -sin(phi) y; 0         sin(phi) cos(phi) z; 0 0 0 1];
 Ty = @(x,y,z,phi) [cos(phi)  0        sin(phi) x; 0         1        0         y; -sin(phi) 0        cos(phi) z; 0 0 0 1];
 Tz = @(x,y,z,phi) [cos(phi) -sin(phi) 0        x; sin(phi)  cos(phi) 0         y; 0         0        1        z; 0 0 0 1];
 
@@ -36,7 +37,7 @@ Tz = @(x,y,z,phi) [cos(phi) -sin(phi) 0        x; sin(phi)  cos(phi) 0         y
 % Body              {b}
 % Space             {s}
 
-% Default Configurations 
+% Default Configurations
 M0e =  [1 0 0 0.0330; 0 1 0 0; 0 0 1 0.6546; 0 0 0 1]; % default End-Effector configuration
 Tb0 =  [1 0 0 0.1667; 0 1 0 0; 0 0 1 0.0026; 0 0 0 1]; % Base to Body frame transformation
 
@@ -51,13 +52,13 @@ Blist = [B1,B2,B3,B4,B5];
 % Chasis z-height
 zsb = 0.0963;
 
-% Joint Velocity Limmits
+% Joint Velocity Limmits (DU,DTHETA)
 speed_max = 1000*[1,1,1,1,1,1,1,1,1];
 
 %% Initial conditions
-q0 = [0,0,0]';                      % position of body (x,y,phi)         
-u0 = [0,0,0,0]';                    % wheel angles     (R^4)
-theta0 = [0,pi/4,-pi/4,-pi/2,0]';   % joint angles     (R^5)
+q0 = [0,0,0]';                         % position of body (x,y,phi)
+u0 = [0,0,0,0]';                       % wheel angles     (R^4)
+theta0 = [0,pi/4,-pi/4,-pi/2,0]';      % joint angles     (R^5)
 
 %% Generate Trajectory
 Tsb = Tz(q0(2),q0(3),zsb,q0(1));       % Body to Spacial frame transformation
@@ -65,22 +66,23 @@ T0e = FKinBody(M0e,Blist,theta0);      % End-Effector to Base transformation
 Tse = Tsb * Tb0 * T0e;                 % End-Effector to Space transformation
 
 % Waypoints
-Tse_i = Tse;                        % initial   configuration of the end-effector   (space frame)
-Tce_s = Ty( 0, 0, .25, pi/2);         % standoff  configuration of the end-effector   (cube frame)
-Tce_g = Ty( 0.01, 0,  0.02, pi/2);         % grasping  configuration of the end-effector   (cube frame)
-Tsc_i = Tz( 1,  0,  0,  0);      % initial   configuration of the cube           (space frame)
-Tsc_f = Tz( 0, -1,  0, -pi/2);      % final     configuration of the cube           (space frame)
+Tse_i = Tse;                           % initial   configuration of the end-effector   (space frame)
+Tce_s = Ty( 0, 0, .25, pi/2);          % standoff  configuration of the end-effector   (cube frame)
+Tce_g = Ty( 0.01, 0,  0.02, pi/2);     % grasping  configuration of the end-effector   (cube frame)
+Tsc_i = Tz( 1,  0,  0,  0);            % initial   configuration of the cube           (space frame)
+Tsc_f = Tz( 0, -1,  0, -pi/2);         % final     configuration of the cube           (space frame)
 
 % Resolution
-dt = 0.01;                          % time step in seconds
-k = 10;                             % frames per time step
+dt = 0.01;                             % time step in seconds
+k = 10;                                % frames per time step
 
 % Generate Trajectory
 trajectory = TrajectoryGenerator(Tse_i,Tsc_i,Tsc_f,Tce_g,Tce_s,k,dt);
 
-%% Control Gains
+% Control Gains
 Kp = 100*eye(6);
 Ki = 0.5*eye(6);
+control = [true true true]
 
 %% Running The Simulation
 % Setting the initial conditions
@@ -91,49 +93,55 @@ theta = theta0;
 % Loop Variables
 N = length(trajectory(:,1));    % Number of simulation steps
 t = linspace(0,(N-1)*dt,N-1)/k; % Simulation time
-Q = zeros(N,13);                % Robot configuration
-Xi = zeros(6,1);                % Integrated error twist
+Q = zeros(N-1,13);              % Robot configuration
+Xe = zeros(6,N-1);              % Error in end-effector frame
+Xs = zeros(6,N-1);              % Error in space frame
+Xi = zeros(6,1);                % Integrated error 
 
 for i = 1:N-1
-T0e = FKinBody(M0e,Blist,theta);
-Tsb = [cos(q(1)) -sin(q(1)) 0 q(2); sin(q(1))  cos(q(1)) 0 q(3); 0 0 1 0.0963; 0 0 0 1]; 
-Tse = Tsb * Tb0 * T0e;
+    % Calculate curent spacial configuration
+    T0e = FKinBody(M0e,Blist,theta);          % end effector to base transformation 
+    Tsb = [cos(q(1)) -sin(q(1)) 0 q(2);       % body to space frame transformation
+           sin(q(1))  cos(q(1)) 0 q(3);
+           0 0 1 0.0963;
+           0 0 0 1];
 
-Xd = [trajectory(i,1:3),trajectory(i,10);trajectory(i,4:6),trajectory(i,11);trajectory(i,7:9),trajectory(i,12);0,0,0,1];
-Xd_n = [trajectory(i+1,1:3),trajectory(i+1,10);trajectory(i+1,4:6),trajectory(i+1,11);trajectory(i+1,7:9),trajectory(i+1,12);0,0,0,1];
+    Tse = Tsb * Tb0 * T0e;                    % end effector to space frame transformation 
+    
+    % desired configuration
+    Xd = [trajectory(i,1:3),trajectory(i,10);trajectory(i,4:6),trajectory(i,11);trajectory(i,7:9),trajectory(i,12);0,0,0,1];
 
-[du,dtheta,Vb,Xe(:,i),Xi] = FeedbackControl(theta,Tse,Xd,Xd_n,Kp,Ki,Xi,dt,[false true false]);
-[q,theta,u] = NextState(q,u,theta,du,dtheta,dt,speed_max);
+    % desired next configuration
+    Xd_n = [trajectory(i+1,1:3),trajectory(i+1,10);trajectory(i+1,4:6),trajectory(i+1,11);trajectory(i+1,7:9),trajectory(i+1,12);0,0,0,1];
+    
+    % Feedback Coontrol
+    [du,dtheta,Vb,Xe(:,i),Xi] = FeedbackControl(theta,Tse,Xd,Xd_n,Kp,Ki,Xi,dt,control);
 
-Q(i,:) = [q;theta;u;trajectory(i,13)]'; 
-
+    % Simulate over 1 timestep
+    [q,theta,u] = NextState(q,u,theta,du,dtheta,dt,speed_max);
+    
+    % Store Solution
+    Q(i,:) = [q;theta;u;trajectory(i,13)]';
+    
+    Xs(:,i) = Adjoint(Tse) * Xe(:,i);
 end
 
-writematrix(Q,'robotmotion.csv')
+writematrix(Q, csvfile + "robotmotion1.csv")
 
-%% Plotting Error
-figure(1)
-set(gcf, 'Position', [10 10 800 400]);
-tgl = tiledlayout(1,2);
-title(tgl,"Error Twist vs. Time")
-subtitle(tgl,"Feed Forward + Kp = 100 + Ki = 0.5")
-
-nexttile(tgl,2)
-plot(t,Xe(4:6,:)',"LineWidth",2)
-legend('vx','vy','vz','Location','northwest')
-title('Linear Velocity')
+%% Plot Error
+figure()
+set(gcf,"Position",[10 10 400 400])
+hold on
+plot(t,Xs(1:3,:)','--',"LineWidth",2)
+plot(t,Xs(4:6,:)',"LineWidth",2)
+hold off
 xlabel("Time (s)")
-ylabel("Velocity (m/s)")
-lx = xlim;
-ly = ylim;
-grid on
-
-nexttile(tgl,1)
-plot(t,Xe(1:3,:)',"LineWidth",2)
-legend('wx','wy','wz','Location','northwest');
-title('Rotational Velocity')
-xlabel("Time (s)")
-ylabel("Angular Velocity (rad/s)")
-xlim(lx);
-ylim(ly);
+xlim([0 max(t)])
+ylabel("Error Magnitude")
+title("Velocity Error vs. Time")
+subtitle({"$\bf{K_{p} =}$ " + Kp(1),"$\bf{K_{i} =}$ " + Ki(1)},"Interpreter","Latex")
+xline(t(2*k/dt+63),'--',"Cube Grabbed");
+xline(t(5*k/dt+2*63),'--',"Cube Placed");
+legend("$\bf{w_x}$","$\bf{w_y}$","$\bf{w_z}$","$\bf{v_x}$","$\bf{v_y}$","$\bf{v_z}$",'Location','northwest',"Interpreter","Latex")
+hold off
 grid on
